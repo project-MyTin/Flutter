b45086dc5f6572240fb97a90605a075734745309
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-04-06 11:51:49 +0900

    code : add search box in screen_routine_list.dart

--- lib/screens/screen_routine_list.dart
+++ lib/screens/screen_routine_list.dart
@@ -3,9 +3,41 @@ import 'package:flutter/material.dart';
 class RoutineListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("루틴 리스트"),
+    Size screenSize = MediaQuery.of(context).size;
+    double width = screenSize.width,
+        height = screenSize.height;
+
+    return SafeArea(
+      child: Scaffold(
+        // appBar: AppBar(
+        //   title: Text("루틴 리스트 페이지"),
+        // ),
+        body: Column(
+          children: <Widget>[
+            SizedBox(
+              width: width,
+              height: height * 0.2,
+              child: Container(
+                child: Column(
+                  children: <Widget>[
+                    Icon(Icons.keyboard_backspace),
+                    Row(
+                      children: <Widget>[
+                        Text("Routine"),
+                        ElevatedButton(
+                            onPressed: () => print("누름"),
+                            child: Text("내 루틴 등록하기")
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                color: Colors.lightBlue,
+              ),
+            ),
+            Text("ㅎㅎ"),
+          ],
+        ),
       ),
     );
   }
