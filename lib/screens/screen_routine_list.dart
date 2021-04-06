30b4f9be673599d1fca9ee0734e9c5f13c404f7a
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-04-06 11:17:05 +0900

    code : add screen_routine_list.dart

---
+++ lib/screens/screen_routine_list.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RoutineListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("루틴 리스트"),
+      ),
+    );
+  }
+}
+
