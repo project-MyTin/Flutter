8d5e256b1a74caf36f9f712ab2093e1026477982
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-04-08 19:16:39 +0900

    code : add BottomAppBar in RoutineRunPage

--- lib/screens/screen_routine_run.dart
+++ lib/screens/screen_routine_run.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:mytin/models/motion_time.dart';
 
-
 class RoutineRunPage extends StatefulWidget {
   RoutineRunPage({Key key}) : super(key: key);
 
@@ -12,12 +11,12 @@ class RoutineRunPage extends StatefulWidget {
 }
 
 class _RoutineRunPageState extends State<RoutineRunPage> {
-
   // 더미 데이터
   static List<MotionTime> motions = [
     MotionTime.fromMap({
       "motionTime": 2,
-      "motionUrl": "https://buddhayana2018504915002.files.wordpress.com/2018/11/417.jpg?w=500",
+      "motionUrl":
+      "https://buddhayana2018504915002.files.wordpress.com/2018/11/417.jpg?w=500",
       "motionName": "산에서 명상",
       "motionCount": 3
     }),
@@ -44,27 +43,29 @@ class _RoutineRunPageState extends State<RoutineRunPage> {
   int currentPart = 1;
   int allPart = motions.length;
   int currentTime = motions[0].motionTime * motions[0].motionCount;
-  String motionUrl = "https://www.korea.kr/newsWeb/resources/attaches/namo/2010.10/26/8216/PYH2010101603890001300.jpg";
+  String motionUrl =
+      "https://www.korea.kr/newsWeb/resources/attaches/namo/2010.10/26/8216/PYH2010101603890001300.jpg";
   Timer timer;
 
   void _motionTimer() {
-    const oneSec = Duration(seconds:1);
-    timer = Timer.periodic(oneSec, (Timer t){
-      if(currentTime < 1) {
-        if(currentPart == allPart) {
+    const oneSec = Duration(seconds: 1);
+    timer = Timer.periodic(oneSec, (Timer t) {
+      if (currentTime < 1) {
+        if (currentPart == allPart) {
           currentPart = allPart;
           t.cancel();
           print("타이머 끝이여");
           return;
         }
-        if(currentPart < allPart) {
+        if (currentPart < allPart) {
           currentPart++;
         }
         setState(() {
-          currentTime = motions[currentPart - 1].motionTime * motions[currentPart - 1].motionCount;
+          currentTime = motions[currentPart - 1].motionTime *
+              motions[currentPart - 1].motionCount;
           motionUrl = motions[currentPart - 1].motionUrl;
         });
-      }else {
+      } else {
         setState(() {
           print(currentTime--);
         });
@@ -86,9 +87,11 @@ class _RoutineRunPageState extends State<RoutineRunPage> {
 
   @override
   Widget build(BuildContext context) {
-    Size screenSize = MediaQuery.of(context).size;
+    Size screenSize = MediaQuery
+        .of(context)
+        .size;
     double width = screenSize.width,
-          height = screenSize.height;
+        height = screenSize.height;
 
     return SafeArea(
       child: Scaffold(
@@ -97,10 +100,7 @@ class _RoutineRunPageState extends State<RoutineRunPage> {
           child: AppBar(
             title: Text(
               motions[currentPart - 1].motionName,
-              style: TextStyle(
-                fontSize: 0.024 * height,
-                color: Colors.white
-              ),
+              style: TextStyle(fontSize: 0.024 * height, color: Colors.white),
             ),
             actions: <Widget>[
               Padding(
@@ -108,13 +108,12 @@ class _RoutineRunPageState extends State<RoutineRunPage> {
                 child: TextButton(
                   onPressed: () => print("종료하기"),
                   child: Padding(
-                    padding: EdgeInsets.fromLTRB(0.03 * width, 0, 0.03 * width, 0),
+                    padding:
+                    EdgeInsets.fromLTRB(0.03 * width, 0, 0.03 * width, 0),
                     child: Text(
                       "종료하기",
                       style: TextStyle(
-                        fontSize: 0.015 * height,
-                        color: Colors.white
-                      ),
+                          fontSize: 0.015 * height, color: Colors.white),
                     ),
                   ),
                   style: ButtonStyle(
@@ -144,8 +143,10 @@ class _RoutineRunPageState extends State<RoutineRunPage> {
                     child: Container(
                       child: SizedBox(
                         child: CircularProgressIndicator(
-                          value:  1 - currentTime
-                              / (motions[currentPart - 1].motionTime * motions[currentPart - 1].motionCount),
+                          value: 1 -
+                              currentTime /
+                                  (motions[currentPart - 1].motionTime *
+                                      motions[currentPart - 1].motionCount),
                           strokeWidth: 16,
                         ),
                         width: 0.4 * width,
@@ -154,9 +155,7 @@ class _RoutineRunPageState extends State<RoutineRunPage> {
                       width: 0.4 * width,
                       height: 0.4 * width,
                       decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.white
-                      ),
+                          shape: BoxShape.circle, color: Colors.white),
                     ),
                   ),
                   Align(
@@ -165,13 +164,10 @@ class _RoutineRunPageState extends State<RoutineRunPage> {
                       mainAxisAlignment: MainAxisAlignment.end,
                       children: <Widget>[
                         Text(
-                            "time",
-                          style: TextStyle(
-                          ),
+                          "time",
+                          style: TextStyle(),
                         ),
-                        Text(
-                            currentTime.toString()
-                        ),
+                        Text(currentTime.toString()),
                       ],
                     ),
                   ),
@@ -180,60 +176,36 @@ class _RoutineRunPageState extends State<RoutineRunPage> {
               width: width,
               height: 0.5 * height,
             ),
-            Container(
-              child: Text(
-                "남은시간",
-                style: TextStyle(
-                    fontSize: 0.05 * width
-                ),
-              ),
-              padding: EdgeInsets.fromLTRB(0, 0.02 * height, 0, 0 * height),
+            Text(
+              motions[currentPart - 1].motionName,
+              style: TextStyle(fontSize: 0.06 * width),
             ),
-            Padding(
-              padding: EdgeInsets.all(0.04 * width),
-              child: Text(
-                (currentTime~/60).toString() + ' : ' + (currentTime%60).toString(),
-                style: TextStyle(
-                  fontSize: 0.15 * width,
-                ),
-              ),
-            ),
-            GestureDetector(
-              onTap: (){
-                print("다음으로 넘어가기!");
-                timer.cancel();
-                setState(() {
-                  currentPart = currentPart < allPart? currentPart+1 : currentPart;
-                  currentTime = motions[currentPart - 1].motionTime;
-                  motionUrl = motions[currentPart - 1].motionUrl;
-                });
-                _motionTimer();
-              },
-              child: Stack(
-                alignment: Alignment.center,
-                children: <Widget>[
-                  SizedBox(
-                    height: 0.06 * height,
-                    width: 0.6 * width,
-                    child: LinearProgressIndicator(
-                      value: 1 - currentTime
-                          / (motions[currentPart - 1].motionTime * motions[currentPart - 1].motionCount),
-                    ),
-                  ),
-                  Text(
-                    "다음으로 넘어가기",
-                    style: TextStyle(
-                        fontSize: 18,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              child: Text(currentPart.toString() + '/' + allPart.toString()),
-              alignment: Alignment.center,
+            Text(
+              motions[currentPart - 1].motionCount.toString() + "회",
+              style: TextStyle(fontSize: 0.05 * width),
             ),
           ],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.grey,
+          child: Row(
+            children: <Widget>[
+              Spacer(flex: 39),
+              Text(
+                currentPart.toString() + "/" + allPart.toString(),
+                textAlign: TextAlign.center,
+              ),
+              Spacer(flex: 30),
+              IconButton(
+                onPressed: () =>
+                    print("넘어가보자 가다가 힘들면 쉬었다 가더라도 손잡고 가보자 같이 가보자"),
+                icon: Icon(
+                  Icons.arrow_forward_ios_outlined,
+                  size: 0.06 * width,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
