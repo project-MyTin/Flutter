20610b5cb14418971f63b15e2e3741b32fcd98d6
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-04-09 13:31:47 +0900

    code : split function in RoutineRunPage

--- lib/screens/screen_routine_run.dart
+++ lib/screens/screen_routine_run.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
-
+import 'package:mytin/dummies/motion_time_dummy.dart';
 import 'package:flutter/material.dart';
-import 'package:mytin/models/motion_time.dart';
+// import 'package:mytin/models/motion_time.dart';
 
 class RoutineRunPage extends StatefulWidget {
   RoutineRunPage({Key key}) : super(key: key);
@@ -11,36 +11,6 @@ class RoutineRunPage extends StatefulWidget {
 }
 
 class _RoutineRunPageState extends State<RoutineRunPage> {
-  // 더미 데이터
-  static List<MotionTime> motions = [
-    MotionTime.fromMap({
-      "motionTime": 2,
-      "motionUrl":
-          "https://buddhayana2018504915002.files.wordpress.com/2018/11/417.jpg?w=500",
-      "motionName": "산에서 명상",
-      "motionCount": 1
-    }),
-    MotionTime.fromMap({
-      "motionTime": 4,
-      "motionUrl": "https://t1.daumcdn.net/cfile/tistory/252D9B3F549283D10D",
-      "motionName": "흑백에서 명상",
-      "motionCount": 1
-    }),
-    MotionTime.fromMap({
-      "motionTime": 3,
-      "motionUrl": "https://i.ytimg.com/vi/uPOarntFwCA/hqdefault.jpg",
-      "motionName": "일어서서 명상",
-      "motionCount": 1
-    }),
-    MotionTime.fromMap({
-      "motionTime": 1,
-      "motionUrl":
-          "https://www.sglba.org.au/wp-content/uploads/2017/05/PLACEHOLDER-Blog-600-x700-pixels.png",
-      "motionName": "나무 등지고 명상",
-      "motionCount": 2
-    })
-  ];
-
   int currentPart = 1;
   int allPart = motions.length;
   int currentTime = motions[0].motionTime * motions[0].motionCount;
@@ -54,27 +24,32 @@ class _RoutineRunPageState extends State<RoutineRunPage> {
   void _motionTimer() {
     const oneSec = Duration(seconds: 1);
     timer = Timer.periodic(oneSec, (Timer t) {
-      if (currentTime < 1) {  // 루틴 시간이 끝났을 경우(남은 휴식 시간이 0 이하 && 남은 루틴 시간이 0 이하)
-        if (currentPart == allPart) { // 루틴 시간이 끝난 것이 마지막 파트인 경우 => 종료
+      if (currentTime < 1) {
+        // 루틴 시간이 끝났을 경우(남은 휴식 시간이 0 이하 && 남은 루틴 시간이 0 이하)
+        if (currentPart == allPart) {
+          // 루틴 시간이 끝난 것이 마지막 파트인 경우 => 종료
           t.cancel();
-        } else if (currentBreakTime < 1) { // 휴식 시간도 끝난 경우 => 다음 파트로
+        } else if (currentBreakTime < 1) {
+          // 휴식 시간도 끝난 경우 => 다음 파트로
           setState(() {
-            currentPart ++;
+            currentPart++;
             isBreakTime = false;
             motionUrl = motions[currentPart - 1].motionUrl;
             currentTime = motions[currentPart - 1].motionTime *
                 motions[currentPart - 1].motionCount;
             currentBreakTime = breakTime;
           });
-        } else { // 휴식 시간은 안 끝난 경우
+        } else {
+          // 휴식 시간은 안 끝난 경우
           setState(() {
             isBreakTime = true;
-            // print(currentBreakTime --);
+            print(currentBreakTime--);
           });
         }
-      } else {  // 루틴 시간이 안 끝난 경우
+      } else {
+        // 루틴 시간이 안 끝난 경우
         setState(() {
-          print(currentTime --);
+          print(currentTime--);
         });
       }
     });
@@ -99,36 +74,7 @@ class _RoutineRunPageState extends State<RoutineRunPage> {
 
     return SafeArea(
       child: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(0.07 * height),
-          child: AppBar(
-            title: Text(
-              motions[currentPart - 1].motionName,
-              style: TextStyle(fontSize: 0.024 * height, color: Colors.white),
-            ),
-            actions: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(0.014 * height),
-                child: TextButton(
-                  onPressed: () => print("종료하기"),
-                  child: Text(
-                    "   종료하기    ",
-                    style: TextStyle(
-                        fontSize: 0.015 * height, color: Colors.white),
-                  ),
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.resolveWith((states) {
-                      return RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
-                        side: BorderSide(color: Colors.white),
-                      );
-                    }),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
+        appBar: buildRoutineRunAppBar(height),
         body: Stack(
           children: [
             Column(
@@ -145,54 +91,14 @@ class _RoutineRunPageState extends State<RoutineRunPage> {
                       ),
                       Align(
                         alignment: Alignment.bottomCenter,
-                        child: Container(
-                          child: SizedBox(
-                            child: CircularProgressIndicator(
-                              value: 1 -
-                                  currentTime /
-                                      (motions[currentPart - 1].motionTime *
-                                          motions[currentPart - 1].motionCount),
-                              strokeWidth: 16,
-                            ),
-                            width: 0.4 * width,
-                            height: 0.4 * width,
-                          ),
-                          width: 0.4 * width,
-                          height: 0.4 * width,
-                          decoration: BoxDecoration(
-                              shape: BoxShape.circle, color: Colors.white),
-                        ),
-                      ),
-                      Align(
-                        alignment: Alignment.bottomCenter,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: <Widget>[
-                            Text(
-                              "time",
-                              style: TextStyle(fontSize: 0.024 * height),
-                            ),
-                            Text(
-                              currentTime.toString(),
-                              style: TextStyle(
-                                  fontSize: 0.06 * height,
-                                  fontWeight: FontWeight.bold,
-                                  color: Color.fromARGB(255, 100, 100, 100)),
-                            ),
-                            SizedBox(
-                              height: 0.1 * width,
-                            )
-                          ],
-                        ),
+                        child: buildTimeProgressIndicator(width, height),
                       ),
                     ],
                   ),
                   width: width,
                   height: 0.64 * height,
                 ),
-                SizedBox(
-                  height: 0.03 * height,
-                ),
+                SizedBox(height: 0.03 * height),
                 Text(
                   motions[currentPart - 1].motionName,
                   style: TextStyle(fontSize: 0.06 * width),
@@ -204,42 +110,126 @@ class _RoutineRunPageState extends State<RoutineRunPage> {
                       color: Color.fromARGB(255, 100, 100, 100)),
                 ),
                 Spacer(),
-                Container(
-                  color: Colors.grey,
-                  height: 0.06 * height,
-                  child: Row(
-                    children: <Widget>[
-                      Spacer(flex: 4),
-                      Text(
-                        currentPart.toString() + "/" + allPart.toString(),
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 0.023 * height),
-                      ),
-                      Spacer(flex: 3),
-                      IconButton(
-                        onPressed: () => print("넘어가보자 가다가 힘들면 쉬었다 가더라도 손잡고 가보자 같이 가보자"),
-                        icon: Icon(
-                          Icons.arrow_forward_ios_outlined,
-                          size: 0.06 * width,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+                buildRoutineRunBottomAppBar(height, width),
               ],
             ),
             Container(
-              color: Color.fromARGB(isBreakTime? 230 : 0, 40, 40, 40),
+              color: Color.fromARGB(isBreakTime ? 230 : 0, 40, 40, 40),
               child: Center(
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-
+                    Text(
+                      "Break Time",
+                      style: TextStyle(
+                          fontSize: 0.06 * width,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white.withOpacity(isBreakTime ? 1 : 0)),
+                    ),
                   ],
                 ),
               ),
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  Container buildRoutineRunBottomAppBar(double height, double width) {
+    return Container(
+      color: Colors.grey,
+      height: 0.06 * height,
+      child: Row(
+        children: <Widget>[
+          Spacer(flex: 4),
+          Text(
+            currentPart.toString() + "/" + allPart.toString(),
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 0.023 * height),
+          ),
+          Spacer(flex: 3),
+          IconButton(
+            onPressed: () => print("다음 파트로"),
+            icon: Icon(
+              Icons.arrow_forward_ios_outlined,
+              size: 0.06 * width,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container buildTimeProgressIndicator(double width, double height) {
+    return Container(
+      child: Stack(
+        children: [
+          SizedBox(
+            child: CircularProgressIndicator(
+              value: 1 -
+                  currentTime /
+                      (motions[currentPart - 1].motionTime *
+                          motions[currentPart - 1].motionCount),
+              strokeWidth: 16,
+            ),
+            width: 0.4 * width,
+            height: 0.4 * width,
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "time",
+                  style: TextStyle(fontSize: 0.024 * height),
+                ),
+                Text(
+                  currentTime.toString(),
+                  style: TextStyle(
+                      fontSize: 0.06 * height,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 100, 100, 100)),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      width: 0.4 * width,
+      height: 0.4 * width,
+      decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.white),
+    );
+  }
+
+  PreferredSize buildRoutineRunAppBar(double height) {
+    return PreferredSize(
+      preferredSize: Size.fromHeight(0.07 * height),
+      child: AppBar(
+        title: Text(
+          motions[currentPart - 1].motionName,
+          style: TextStyle(fontSize: 0.024 * height, color: Colors.white),
+        ),
+        actions: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(0.014 * height),
+            child: TextButton(
+              onPressed: () => print("종료하기"),
+              child: Text(
+                "   종료하기    ",
+                style: TextStyle(fontSize: 0.015 * height, color: Colors.white),
+              ),
+              style: ButtonStyle(
+                shape: MaterialStateProperty.resolveWith((states) {
+                  return RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    side: BorderSide(color: Colors.white),
+                  );
+                }),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
