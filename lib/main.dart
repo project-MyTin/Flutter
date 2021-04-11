1a0592c1fc20a81b8a266c9cf1ed170817f35ff5
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-04-11 23:07:18 +0900

    code : add AppBar, BottomBar in screens_routine_detail.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:mytin/models/routine_detail.dart';
 import 'package:mytin/screens/screen_routine_list.dart';
 import 'package:mytin/screens/screen_routine_run.dart';
+import 'package:mytin/screens/screens_routine_detail.dart';
 
 
 void main() {
@@ -16,7 +18,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.grey,
       ),
-      home: RoutineRunPage(),
+      // home: RoutineRunPage(),
+      home: RoutineDetailPage(),
       // home: RoutineListPage(),
     );
   }
--- lib/screens/screens_routine_detail.dart
+++ lib/screens/screens_routine_detail.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mytin/dummies/routine_detail_dummy.dart';
 
-class RoutineDetail extends StatelessWidget {
-
+class RoutineDetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +12,24 @@ class RoutineDetail extends StatelessWidget {
     return SafeArea(
         child: Scaffold(
           appBar: AppBar(
-            title: Text(""),
+            title: Text(routine.name),
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back_ios_outlined),
+              onPressed: () => print("나 나갈래!"),
+            ),
+            actions: [
+              IconButton(icon: Icon(Icons.delete), onPressed: () => print("지울래!")),
+              IconButton(icon: Icon(Icons.edit), onPressed: () => print("수정할래!"))
+            ],
+          ),
+          body: Column(
+            children: [
+
+            ],
+          ),
+          bottomNavigationBar: TextButton(
+            child: Text("시작하기"),
+            onPressed: () => print("시작좀 하자!"),
           ),
         )
     );
