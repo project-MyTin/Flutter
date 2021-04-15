739e16cf3645c98968df3700f4a65e6169904db9
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-04-15 17:59:47 +0900

    code : add RoutineAddHeader

--- lib/screens/screen_routine_add.dart
+++ lib/screens/screen_routine_add.dart
@@ -6,12 +6,45 @@ class RoutineAddPage extends StatelessWidget {
     Size screenSize = MediaQuery.of(context).size;
     double width = screenSize.width, height = screenSize.height;
 
-    return Scaffold(
-      body: Column(
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
           children: [
-            Text("루틴 등록하기", style: TextStyle(fontSize: 0.03 * height),)
+            RoutineAddHeader(height),
+
           ],
+        ),
       ),
+    );
+  }
+}
+
+
+class RoutineAddHeader extends StatelessWidget {
+  final double height;
+
+  RoutineAddHeader(this.height);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Align(
+          alignment: Alignment.topLeft,
+          child: IconButton(
+            icon: Icon(Icons.arrow_back_ios),
+            onPressed: () {},
+          ),
+        ),
+        Text(
+          "루틴 등록하기",
+          style: TextStyle(fontSize: 0.04 * height),
+        ),
+        Text(
+          "원하는 동작을 추가해주세요",
+          style: TextStyle(fontSize: 0.018 * height),
+        )
+      ],
     );
   }
 }
