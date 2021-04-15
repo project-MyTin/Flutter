b79250a6fa03c07bdef2b42ec36e0e68537a3527
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-04-15 16:10:46 +0900

    refactor : edit buildBreakTimeBody

--- lib/screens/screen_routine_run.dart
+++ lib/screens/screen_routine_run.dart
@@ -85,8 +85,9 @@ class _RoutineRunPageState extends State<RoutineRunPage> {
                       controller.motionCount, height, width),
                 ],
               ),
-              buildBreakTimeBody(controller.currentBreakTime,
-                  controller.breakTime, controller.isBreakTime, height, width),
+              if (controller.isBreakTime)
+                buildBreakTimeBody(controller.currentBreakTime,
+                    controller.breakTime, height, width),
             ],
           ),
         ),
@@ -94,74 +95,71 @@ class _RoutineRunPageState extends State<RoutineRunPage> {
     );
   }
 
-  Container buildBreakTimeBody(int currentBreakTime, int breakTime,
-      bool isBreakTime, double height, double width) {
-    if (isBreakTime) {
-      return Container(
-        color: Color.fromARGB(240, 40, 40, 40),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Break Time",
-                style: TextStyle(
-                    fontSize: 0.06 * width,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white.withOpacity(isBreakTime ? 1 : 0)),
+  Container buildBreakTimeBody(
+      int currentBreakTime, int breakTime, double height, double width) {
+    return Container(
+      color: Color.fromARGB(240, 40, 40, 40),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Break Time",
+              style: TextStyle(
+                fontSize: 0.06 * width,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
               ),
-              SizedBox(height: 0.03 * height),
-              GetBuilder<CountdownController>(
-                builder: (controller) => TimeProgressIndicator(
-                  width: width * 1.1,
-                  height: height * 1.1,
-                  currentTime: controller.currentBreakTime,
-                  allTime: controller.breakTime,
-                  textColor: Colors.white,
-                  backgroundColor: Color.fromARGB(255, 40, 40, 40),
+            ),
+            SizedBox(height: 0.03 * height),
+            GetBuilder<CountdownController>(
+              builder: (controller) => TimeProgressIndicator(
+                width: width * 1.1,
+                height: height * 1.1,
+                currentTime: controller.currentBreakTime,
+                allTime: controller.breakTime,
+                textColor: Colors.white,
+                backgroundColor: Color.fromARGB(255, 40, 40, 40),
+              ),
+            ),
+            SizedBox(height: 0.04 * height),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () => _controller.addBreakTime(5),
+                  child: Text("5초 연장하기"),
+                  style: ButtonStyle(
+                    shadowColor: MaterialStateProperty.resolveWith(
+                        (states) => Color.fromARGB(255, 40, 40, 40)),
+                    shape: MaterialStateProperty.resolveWith((states) {
+                      return RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        side:
+                            BorderSide(color: Color.fromARGB(255, 40, 40, 40)),
+                      );
+                    }),
+                  ),
                 ),
-              ),
-              SizedBox(height: 0.04 * height),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    onPressed: () => _controller.addBreakTime(5),
-                    child: Text("5초 연장하기"),
-                    style: ButtonStyle(
-                      shadowColor: MaterialStateProperty.resolveWith(
-                          (states) => Color.fromARGB(255, 40, 40, 40)),
-                      shape: MaterialStateProperty.resolveWith((states) {
-                        return RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                          side: BorderSide(
-                              color: Color.fromARGB(255, 40, 40, 40)),
-                        );
-                      }),
-                    ),
+                SizedBox(width: 0.08 * width),
+                TextButton(
+                  onPressed: () => _controller.passBreakTime(),
+                  child: Text("넘어가기"),
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.resolveWith((states) {
+                      return RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        side: BorderSide(color: Colors.grey),
+                      );
+                    }),
                   ),
-                  SizedBox(width: 0.08 * width),
-                  TextButton(
-                    onPressed: () => _controller.passBreakTime(),
-                    child: Text("넘어가기"),
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.resolveWith((states) {
-                        return RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                          side: BorderSide(color: Colors.grey),
-                        );
-                      }),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
+                ),
+              ],
+            ),
+          ],
         ),
-      );
-    } else {
-      return Container();
-    }
+      ),
+    );
   }
 
   Container buildRoutineRunBottomAppBar(
