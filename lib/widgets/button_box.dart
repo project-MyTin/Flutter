d4a2f380397513279f45dd3975d14d666edc8c6f
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-04-22 19:17:41 +0900

    code : edit ButtonBox be stateful

--- lib/widgets/button_box.dart
+++ lib/widgets/button_box.dart
@@ -1,32 +1,41 @@
 import 'package:flutter/material.dart';
 
-class ButtonBox extends StatelessWidget {
+class ButtonBox extends StatefulWidget {
   final String name;
+  // final Function func;
 
   ButtonBox(this.name);
 
+  @override
+  _ButtonBoxState createState() => _ButtonBoxState();
+}
+
+class _ButtonBoxState extends State<ButtonBox> {
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
     double width = screenSize.width;
 
-    return Container(
-      margin: EdgeInsets.all(0.01 * width),
-      padding: EdgeInsets.fromLTRB(
-          0.05 * width, 0.02 * width, 0.05 * width, 0.02 * width),
-      child: Text(name),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(15)),
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
-            spreadRadius: 0.6,
-            blurRadius: 1,
-            offset: Offset(0, 1),
-          ),
-        ],
+    return GestureDetector(
+      child: Container(
+        margin: EdgeInsets.all(0.01 * width),
+        padding: EdgeInsets.fromLTRB(
+            0.05 * width, 0.02 * width, 0.05 * width, 0.02 * width),
+        child: Text(widget.name),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(15)),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 0.6,
+              blurRadius: 1,
+              offset: Offset(0, 1),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
