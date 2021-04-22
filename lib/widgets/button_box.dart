29910234d7f0e6a46f303232f31cb11c5734a97d
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-04-22 19:29:57 +0900

    code : add clicked UI in ButtonBox

--- lib/widgets/button_box.dart
+++ lib/widgets/button_box.dart
@@ -11,6 +11,8 @@ class ButtonBox extends StatefulWidget {
 }
 
 class _ButtonBoxState extends State<ButtonBox> {
+  bool isClicked = false;
+
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
@@ -21,21 +23,29 @@ class _ButtonBoxState extends State<ButtonBox> {
         margin: EdgeInsets.all(0.01 * width),
         padding: EdgeInsets.fromLTRB(
             0.05 * width, 0.02 * width, 0.05 * width, 0.02 * width),
-        child: Text(widget.name),
+        child: Text(
+          widget.name,
+          style: TextStyle(color: isClicked ? Colors.white : Colors.black87),
+        ),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.all(Radius.circular(15)),
-          color: Colors.white,
+          color: isClicked ? Colors.black54 : Colors.white,
           boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.5),
-              spreadRadius: 0.6,
-              blurRadius: 1,
-              offset: Offset(0, 1),
-            ),
+            if (!isClicked)
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 0.6,
+                blurRadius: 1,
+                offset: Offset(0, 1),
+              ),
           ],
         ),
       ),
+      onTap: () {
+        setState(() {
+          isClicked = !isClicked;
+        });
+      },
     );
   }
 }
-
