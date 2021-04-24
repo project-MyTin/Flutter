42319f8119a07c7188e5f1b827153334811299a8
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-04-25 00:22:18 +0900

    code : delete legacy code in screen_routine_list.dart

--- lib/screens/screen_routine_list.dart
+++ lib/screens/screen_routine_list.dart
@@ -14,55 +14,10 @@ class RoutineListPage extends StatelessWidget {
         // ),
         body: Column(
           children: <Widget>[
-            SizedBox(
-              width: width,
-              height: height * 0.2,
-              child: Container(
-                child: Column(
-                  children: <Widget>[
-                    Icon(
-                      Icons.keyboard_backspace,
-                      color: Colors.white,
-                    ),
-                    Row(
-                      children: <Widget>[
-                        Text(
-                          "Routine",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 0.08 * width
-                          ),
-                        ),
-                        ElevatedButton(
-                            onPressed: () => print("누름"),
-                            child: Text("내 루틴 등록하기")
-                        ),
-                      ],
-                    ),
-                    Text(
-                      "Share your routine with people",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 0.04 * width
-                      ),
-                    ),
-                  ],
-                ),
-                color: Colors.lightBlue,
-              ),
+
+            Container(
+              child: Text(""),
             ),
-            ListView.separated(
-                itemBuilder: (BuildContext context, int index){
-                  return ListTile(
-                    title: Text(index.toString()),
-                  );
-                },
-                separatorBuilder: (context, index) {
-                  if (index == 0) return SizedBox.shrink();
-                  return const Divider();
-                },
-                itemCount: 2,
-            )
           ],
         ),
       ),
