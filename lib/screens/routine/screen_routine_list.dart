02e6590e9b78f3730b1f3ca02e7639a40fbfd350
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-04-25 17:30:34 +0900

    code : apply ListViewHeader

--- lib/screens/routine/screen_routine_list.dart
+++ lib/screens/routine/screen_routine_list.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mytin/dummies/routine_list_dummy.dart';
-import 'package:mytin/widgets/content_list_header.dart';
+import 'package:mytin/widgets/list_view_header.dart';
 import 'package:mytin/widgets/routine/routine_list_tile.dart';
 
 class RoutineListPage extends StatelessWidget {
@@ -15,7 +15,7 @@ class RoutineListPage extends StatelessWidget {
           child: ListView.builder(
             itemCount: routineList.length + 1,
             itemBuilder: (context, index) => (index == 0)
-                ? ContentListHeader("Routine", "다양한 운동 루틴을 수행하고 직접 만들어 공유해보세요!", () {})
+                ? ListViewHeader("Routine", "다양한 운동 루틴을 수행하고 직접 만들어 공유해보세요!", () {})
                 : RoutineListTile(routineList[index - 1]),
           ),
           color: Colors.black.withOpacity(0.02),
--- lib/widgets/list_view_header.dart
+++ lib/widgets/list_view_header.dart
@@ -1,37 +1,37 @@
 import 'package:flutter/material.dart';
 
-class SliverListHeader extends StatelessWidget {
+class ListViewHeader extends StatelessWidget {
   final String title, subText;
   final Function searchFunc;
 
-  SliverListHeader(this.title, this.subText, this.searchFunc);
+  ListViewHeader(this.title, this.subText, this.searchFunc);
 
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
     double height = screenSize.height, width = screenSize.width;
 
-    return SliverToBoxAdapter(
-      child: Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            IconButton(icon: Icon(Icons.keyboard_backspace), onPressed: () {}),
-            Padding(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(title, style: TextStyle(fontSize: 0.036 * height, fontWeight: FontWeight.bold)),
-                  Text(subText),
-                ],
-              ),
-              padding: EdgeInsets.fromLTRB(0.03 * width, 0, 0, 0),
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          IconButton(icon: Icon(Icons.keyboard_backspace), onPressed: () {}),
+          Padding(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(title,
+                    style: TextStyle(
+                        fontSize: 0.036 * height, fontWeight: FontWeight.bold)),
+                Text(subText),
+              ],
             ),
-          ],
-        ),
-        color: Colors.white,
-        padding: EdgeInsets.all(0.03 * width),
+            padding: EdgeInsets.fromLTRB(0.03 * width, 0, 0, 0),
+          ),
+        ],
       ),
+      color: Colors.white,
+      padding: EdgeInsets.all(0.03 * width),
     );
   }
 }
