525e2c28851fd6471320774f98b7052cfd0f1c57
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-04-25 23:57:41 +0900

    code : add comment in add_page_header.dart

--- lib/widgets/add_page_header.dart
+++ lib/widgets/add_page_header.dart
@@ -1,3 +1,14 @@
+/*
+ 루틴 & 동작의 생성 & 수정하기 페이지의 헤더를 만드는 위젯
+ @title : 제목
+ @subText : 제목 밑의 부가 설명
+ @part : 페이지의 현재 단계
+ @backPageFunc : 뒤로가기 버튼을 눌렀을 때 이벤트 핸들러
+ @pageNumbering : ProgressPageIcon 이 현제 단계를 표시하는 기준이 되는 리스트
+
+ (-> 페이지의 현재 단계와 사용자에게 보여지는 단계가 다를 수 있음,
+ 실제 단계는 part 변수에 담기고, 사용자에게 보여지는 단계 리스트는 pageNumbering 변수에 담김)
+ */
 import 'package:flutter/material.dart';
 import 'package:mytin/widgets/progress_page_icon.dart';
 
