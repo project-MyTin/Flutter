a07c7ff2fdfa2b6f78a00728c54031d291af20d3
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-04-25 20:16:36 +0900

    code : implement motion_grid_tile.dart

--- lib/widgets/motion/motion_grid_tile.dart
+++ lib/widgets/motion/motion_grid_tile.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:mytin/models/motion_tile.dart';
-import 'package:mytin/widgets/image_circular.dart';
 
 class MotionGridTile extends StatelessWidget {
   final MotionTile motion;
@@ -11,35 +11,52 @@ class MotionGridTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
-    double width = screenSize.width, height = screenSize.height;
+    double width = screenSize.width;
 
     return Container(
-      child: Container(
-        child: Row(
-          children: [
-            Text("  " + motion.name, style: TextStyle(
-              color: Colors.white,
-              fontSize: 0.04 * width
-            )),
-          ],
-        ),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.all(Radius.circular(15)),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.4),
-              blurRadius: 1,
-              spreadRadius: 1,
-              offset: Offset(0, 1),
-            ),
-          ],
-          image: DecorationImage(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(motion.name,
+              style: TextStyle(color: Colors.white, fontSize: 0.04 * width)),
+          Text(motion.part,
+              style: TextStyle(
+                  color: Colors.white.withOpacity(0.7),
+                  fontSize: 0.03 * width)),
+          Expanded(child: Container()),
+          buildDifficultyBox(width),
+        ],
+      ),
+      padding: EdgeInsets.all(0.02 * width),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(15)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.4),
+            blurRadius: 1,
+            spreadRadius: 1,
+            offset: Offset(0, 1),
+          ),
+        ],
+        image: DecorationImage(
             image: NetworkImage(motion.imageUrl),
             fit: BoxFit.cover,
-            colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.4), BlendMode.darken)
-          ),
-        ),
+            colorFilter: ColorFilter.mode(
+                Colors.black.withOpacity(0.4), BlendMode.darken)),
+      ),
+    );
+  }
+
+  Container buildDifficultyBox(double width) {
+    return Container(
+      child: Text(motion.difficulty,
+          style: TextStyle(color: Colors.white, fontSize: 0.025 * width)),
+      alignment: Alignment.center,
+      height: 0.04 * width,
+      width: 0.07 * width,
+      decoration: BoxDecoration(
+        color: Colors.black.withOpacity(0.6),
+        borderRadius: BorderRadius.all(Radius.circular(3)),
       ),
     );
   }
