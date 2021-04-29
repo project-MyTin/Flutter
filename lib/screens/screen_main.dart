6732450f5a8da4ea7d1990c73c9ff4c3d36cdd24
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-04-29 21:59:11 +0900

    code : edit style in MainPageHeader

--- lib/screens/screen_main.dart
+++ lib/screens/screen_main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:mytin/widgets/page_bottom_navigation_bar.dart';
 
 class MainPage extends StatelessWidget {
@@ -25,27 +26,42 @@ class MainPage extends StatelessWidget {
   Container buildMainPageHeader(double height, double width) {
     return Container(
       width: width,
-      height: 0.4 * height,
+      height: 0.6 * height,
       child: Column(
-            children: [
-              SizedBox(height: 0.09 * height),
-              Text("안녕하세요!", style: TextStyle(color: Colors.white, fontSize: 0.03 * height)),
-              SizedBox(height: 0.02 * height),
-              Text("포근해지는 날씨를 따라서,", style: TextStyle(color: Colors.white, fontSize: 0.022 * height), textAlign: TextAlign.center,),
-              Text("오늘도 새로운 루틴과 동작이 추가되었어요", style: TextStyle(color: Colors.white, fontSize: 0.022 * height), textAlign: TextAlign.center,),
-              SizedBox(height: 0.03 * height),
-              Text("한번 활기차게 운동해볼까요?", style: TextStyle(color: Colors.white, fontSize: 0.024 * height)),
-            ],
+        children: [
+          SizedBox(height: 0.05 * height),
+          SvgPicture.asset("images/undraw_fitness_tracker_3033.svg", height: 0.2 * height),
+          SizedBox(height: 0.04 * height),
+          Text("안녕하세요!",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 0.03 * height,
+                  fontWeight: FontWeight.bold)),
+          SizedBox(height: 0.01 * height),
+          Text(
+            "포근해지는 날씨를 맞아,",
+            style: TextStyle(color: Colors.white, fontSize: 0.02 * height),
+            textAlign: TextAlign.center,
           ),
+          Text(
+            "오늘도 새로운 루틴과 동작이 추가되었어요",
+            style: TextStyle(color: Colors.white, fontSize: 0.02 * height),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 0.02 * height),
+          Text("이번에도 한번 힘내서 운동해볼까요?",
+              style: TextStyle(color: Colors.white, fontSize: 0.022 * height)),
+          SizedBox(height: 0.01 * height),
+        ],
+      ),
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          begin: Alignment.centerLeft,
-          end: Alignment.centerRight,
-          colors: [
-            Colors.indigo,
-            Colors.deepPurple
-          ]
-        ),
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+            colors: [
+              Colors.cyanAccent,
+              Colors.lightBlue,
+            ]),
       ),
     );
   }
