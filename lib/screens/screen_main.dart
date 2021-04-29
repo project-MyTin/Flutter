468156905a1785e9bfb445ccedb771296d576f9d
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-04-29 20:24:53 +0900

    code : add text in MainPageHeader

--- lib/screens/screen_main.dart
+++ lib/screens/screen_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:mytin/widgets/page_bottom_navigation_bar.dart';
 
@@ -24,36 +25,18 @@ class MainPage extends StatelessWidget {
 
   Container buildMainPageHeader(double height, double width) {
     return Container(
-      color: Colors.blue.withOpacity(0.3),
+      color: Colors.indigo,
       width: width,
       height: 0.4 * height,
       child: Column(
             children: [
-              SizedBox(height: 0.04 * height),
-              SvgPicture.asset(
-                "images/undraw_fitness_tracker_3033.svg",
-                width: 0.7 * width,
-              ),
-              Container(
-                // decoration: ,
-              ),
-              // Stack(
-              //   children: [
-              //     // Positioned(
-              //     //   top: 0.6 * height,
-              //     //   child: Container(
-              //     //     child: Text("d"),
-              //     //     height: 0.4 * height,
-              //     //     width: width,
-              //     //     decoration: BoxDecoration(
-              //     //       color: Colors.white,
-              //     //       borderRadius: BorderRadius.all(Radius.circular(20)),
-              //     //     ),
-              //     //   ),
-              //     // ),
-              //   ],
-              // ),
-
+              SizedBox(height: 0.09 * height),
+              Text("안녕하세요!", style: TextStyle(color: Colors.white, fontSize: 0.03 * height)),
+              SizedBox(height: 0.02 * height),
+              Text("포근해지는 날씨를 따라서,", style: TextStyle(color: Colors.white, fontSize: 0.022 * height), textAlign: TextAlign.center,),
+              Text("오늘도 새로운 루틴과 동작이 추가되었어요", style: TextStyle(color: Colors.white, fontSize: 0.022 * height), textAlign: TextAlign.center,),
+              SizedBox(height: 0.03 * height),
+              Text("한번 활기차게 운동해볼까요?", style: TextStyle(color: Colors.white, fontSize: 0.024 * height)),
             ],
           ),
     );
