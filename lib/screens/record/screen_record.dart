3dd0fc56cb1fe6bfd6ea75b04f5b8e364a6b691f
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-05-25 23:44:07 +0900

    code : add main_data_record.dart

--- lib/screens/record/screen_record.dart
+++ lib/screens/record/screen_record.dart
@@ -1,23 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:mytin/widgets/page_bottom_navigation_bar.dart';
+import 'package:mytin/widgets/record/main_data_record.dart';
 
 class RecordPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(
-      body: Column(
-        children: [
-          Row(
-            children: [
-              IconButton(icon: Icon(Icons.arrow_back_ios), onPressed: () {}),
-              Text("5월"),
-              Text("2021년"),
-              IconButton(icon: Icon(Icons.arrow_forward_ios), onPressed: () {}),
-            ],
-          )
-        ],
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Row(
+              children: [
+                IconButton(icon: Icon(Icons.arrow_back_ios), onPressed: () {}),
+                Text("5월"),
+                Text("2021년"),
+                IconButton(
+                    icon: Icon(Icons.arrow_forward_ios), onPressed: () {}),
+                Spacer(),
+                TextButton(onPressed: () {}, child: Text("D")),
+                TextButton(onPressed: () {}, child: Text("W")),
+                TextButton(onPressed: () {}, child: Text("M")),
+              ],
+            ),
+            MainDataRecord({
+              "totalTime": 10,
+              "totalExp": 10,
+              "countRoutine": 10,
+              "countMotion": 10,
+            }),
+          ],
+        ),
+        bottomNavigationBar: PageBottomNavigationBar(),
       ),
-      bottomNavigationBar: PageBottomNavigationBar(),
-    ));
+    );
   }
 }
---
+++ lib/widgets/record/main_data_record.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class MainDataRecord extends StatelessWidget {
+  final List<int> dataList;
+  final List<List<String>> nameList = [
+    ["루틴 수행시간", "분"],
+    ["총 경험치", "exp"],
+    ["루틴 횟수", "개"],
+    ["동작 횟수", "개"],
+  ];
+
+  MainDataRecord(Map<String, int> map) :
+      dataList = [
+        map["totalTime"],
+        map["totalExp"],
+        map["countRoutine"],
+        map["countMotion"]
+      ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(height: Get.height * 0.03),
+        Stack(
+          alignment: Alignment.center,
+          children: [
+            SizedBox(
+              child: CircularProgressIndicator(
+                value: 1,
+                strokeWidth: 22,
+              ),
+              height: Get.width * 0.35,
+              width: Get.width * 0.35,
+            ),
+            Column(
+              children: [
+                Text(nameList[0][0]),
+                Text(dataList[0].toString()),
+                Text(nameList[0][1]),
+              ],
+            ),
+          ],
+        ),
+        SizedBox(height: Get.height * 0.05),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            for (int i = 1; i < dataList.length; i++)
+              Column(
+                children: [
+                  Text(nameList[i][0]),
+                  Text(dataList[i].toString() + " " + nameList[i][1]),
+                ],
+              ),
+          ],
+        )
+      ],
+    );
+  }
+}
