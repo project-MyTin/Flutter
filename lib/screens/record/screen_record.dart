76d30003226f831c245f558c1599f84bc704268a
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-05-25 19:27:37 +0900

    code : add screen_record.dart

---
+++ lib/screens/record/screen_record.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:mytin/widgets/page_bottom_navigation_bar.dart';
+
+class RecordPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Scaffold(
+      body: Column(
+        children: [
+          Row(
+            children: [
+              IconButton(icon: Icon(Icons.arrow_back_ios), onPressed: () {}),
+              Text("5월"),
+              Text("2021년"),
+              IconButton(icon: Icon(Icons.arrow_forward_ios), onPressed: () {}),
+            ],
+          )
+        ],
+      ),
+      bottomNavigationBar: PageBottomNavigationBar(3),
+    ));
+  }
+}
