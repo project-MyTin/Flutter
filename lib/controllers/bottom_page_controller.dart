650770e6d8e6b8bd4a3bce58a83d90789d16a898
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-05-26 10:32:41 +0900

    code : add screen_rank.dart

--- lib/controllers/bottom_page_controller.dart
+++ lib/controllers/bottom_page_controller.dart
@@ -1,6 +1,6 @@
 import 'package:get/get.dart';
 import 'package:mytin/screens/profile/screen_profile_home.dart';
-import 'package:mytin/screens/rank/screen_rank.dart';
+import 'package:mytin/screens/rank/screen_rank_tabs.dart';
 import 'package:mytin/screens/record/screen_record.dart';
 import 'package:mytin/screens/screen_main.dart';
 import 'package:mytin/screens/screen_routine_and_motion.dart';
@@ -21,7 +21,7 @@ class BottomPageController extends GetxController {
         Get.offAll(RecordPage());
         break;
       case 3:
-        Get.offAll(RankPage());
+        Get.offAll(RankTabsPage());
         break;
       case 4:
         Get.offAll(ProfileHomePage());
--- lib/screens/rank/screen_rank.dart
+++ lib/screens/rank/screen_rank.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:mytin/widgets/page_bottom_navigation_bar.dart';
 
 class RankPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
+    return Column(
+      children: [
+        Row(
+          children: [
 
-        ],
-      ),
-      bottomNavigationBar: PageBottomNavigationBar(),
+          ],
+        ),
+      ],
     );
   }
 }
