2664241d8baccd1d8896ab4fcbcfd14d6b811d28
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-05-26 11:17:19 +0900

    code : add rank_list_dummy.dart

---
+++ lib/dummies/rank_list_dummy.dart
@@ -0,0 +1,64 @@
+import 'package:mytin/models/rank_tile.dart';
+
+final List<RankTile> rankTileList = [
+  RankTile.fromMap({
+    "name": "으뜸이",
+    "imageUrl": "https://www.sciencetimes.co.kr/wp-content/uploads/2014/04/0000057311_008.jpg",
+    "rankNumber": 1,
+    "value": 700,
+  }),
+  RankTile.fromMap({
+    "name": "버금이",
+    "imageUrl": "https://www.sciencetimes.co.kr/wp-content/uploads/2014/04/0000057311_008.jpg",
+    "rankNumber": 2,
+    "value": 650,
+  }),
+  RankTile.fromMap({
+    "name": "버금딸림이",
+    "imageUrl": "https://www.sciencetimes.co.kr/wp-content/uploads/2014/04/0000057311_008.jpg",
+    "rankNumber": 3,
+    "value": 600,
+  }),
+  RankTile.fromMap({
+    "name": "어중이",
+    "imageUrl": "https://www.sciencetimes.co.kr/wp-content/uploads/2014/04/0000057311_008.jpg",
+    "rankNumber": 4,
+    "value": 500,
+  }),
+  RankTile.fromMap({
+    "name": "떠중이",
+    "imageUrl": "https://www.sciencetimes.co.kr/wp-content/uploads/2014/04/0000057311_008.jpg",
+    "rankNumber": 5,
+    "value": 450,
+  }),
+  RankTile.fromMap({
+    "name": "아무개",
+    "imageUrl": "https://www.sciencetimes.co.kr/wp-content/uploads/2014/04/0000057311_008.jpg",
+    "rankNumber": 6,
+    "value": 400,
+  }),
+  RankTile.fromMap({
+    "name": "아무개",
+    "imageUrl": "https://www.sciencetimes.co.kr/wp-content/uploads/2014/04/0000057311_008.jpg",
+    "rankNumber": 7,
+    "value": 400,
+  }),
+  RankTile.fromMap({
+    "name": "아무개",
+    "imageUrl": "https://www.sciencetimes.co.kr/wp-content/uploads/2014/04/0000057311_008.jpg",
+    "rankNumber": 8,
+    "value": 400,
+  }),
+  RankTile.fromMap({
+    "name": "아무개",
+    "imageUrl": "https://www.sciencetimes.co.kr/wp-content/uploads/2014/04/0000057311_008.jpg",
+    "rankNumber": 9,
+    "value": 400,
+  }),
+  RankTile.fromMap({
+    "name": "아무개",
+    "imageUrl": "https://www.sciencetimes.co.kr/wp-content/uploads/2014/04/0000057311_008.jpg",
+    "rankNumber": 10,
+    "value": 300,
+  }),
+];
