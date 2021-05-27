1e011b8dac1c26b91b029b53eb890f691ad18087
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-05-27 23:16:07 +0900

    code : add calendar_data.dart

---
+++ lib/dummies/calendar_data_dummy.dart
@@ -0,0 +1,7 @@
+import 'package:mytin/models/calendar_data.dart';
+
+final CalendarData calendarData = CalendarData([
+  10, 30, 20, 80, 50, 30, 40, 10, 0, 0,
+  60, 70, 60, 80, 10, 20, 40, 10, 10, 30,
+  10, 20, 20, 80, 10, 30, 10, 10, 90, 80, 40
+]);
