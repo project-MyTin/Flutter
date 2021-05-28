351a460a9201fb3f2821ae4f3b7d19c8999d1a2b
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-05-28 12:07:26 +0900

    code : remove legacy Stack wrapper-widget

--- lib/screens/motion/screen_motion_list.dart
+++ lib/screens/motion/screen_motion_list.dart
@@ -10,54 +10,54 @@ class MotionListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Get.put(MotionListController());
-    return Stack(children: [
-      Container(
-        color: Colors.black.withOpacity(0.02),
-        child: CustomScrollView(
-          slivers: [
-            SliverToBoxAdapter(
-              child: TitlePageHeader(
-                  "Motion", "많은 운동 동작들을 찾아보고 직접 만들어 공유해보세요!", () {}),
-            ),
-            GetBuilder<MotionListController>(
-                builder: (ctr) => ctr.motionList.length > 0
-                    ? SliverPadding(
-                        padding: EdgeInsets.all(0.04 * Get.width),
-                        sliver: SliverGrid(
-                          delegate: SliverChildBuilderDelegate(
-                            (context, index) {
-                              return MotionGridTile(
-                                index,
-                                ctr.motionList[index],
-                                Get.dialog,
-                                MotionDetailDialog(currentMotion),
-                                false,
-                              );
-                            },
-                            childCount: ctr.motionList.length,
-                          ),
-                          gridDelegate:
-                              SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2,
-                            childAspectRatio: 5 / 3,
-                            mainAxisSpacing: 0.03 * Get.width,
-                            crossAxisSpacing: 0.03 * Get.width,
-                          ),
-                        ),
-                      )
-                    : SliverToBoxAdapter(
-                        child: Container(
-                          alignment: Alignment.topCenter,
-                          padding: EdgeInsets.only(top: Get.height * 0.07),
-                          child: Text("동작이 텅 비여있습니다?",
-                              style: TextStyle(
-                                  color: Colors.grey.withOpacity(0.6),
-                                  fontSize: Get.height * 0.022)),
-                        ),
-                      )),
-          ],
-        ),
+    return Container(
+      color: Colors.black.withOpacity(0.02),
+      child: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: TitlePageHeader(
+                "Motion", "많은 운동 동작들을 찾아보고 직접 만들어 공유해보세요!", () {}),
+          ),
+          GetBuilder<MotionListController>(
+            builder: (ctr) => ctr.motionList.length > 0
+                ? SliverPadding(
+                    padding: EdgeInsets.all(0.04 * Get.width),
+                    sliver: SliverGrid(
+                      delegate: SliverChildBuilderDelegate(
+                        (context, index) {
+                          return MotionGridTile(
+                            index,
+                            ctr.motionList[index],
+                            Get.dialog,
+                            MotionDetailDialog(currentMotion),
+                            false,
+                          );
+                        },
+                        childCount: ctr.motionList.length,
+                      ),
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        childAspectRatio: 5 / 3,
+                        mainAxisSpacing: 0.03 * Get.width,
+                        crossAxisSpacing: 0.03 * Get.width,
+                      ),
+                    ),
+                  )
+                : SliverToBoxAdapter(
+                    child: Container(
+                      alignment: Alignment.topCenter,
+                      padding: EdgeInsets.only(top: Get.height * 0.07),
+                      child: Text(
+                        "동작이 텅 비여있습니다?",
+                        style: TextStyle(
+                            color: Colors.grey.withOpacity(0.6),
+                            fontSize: Get.height * 0.022),
+                      ),
+                    ),
+                  ),
+          ),
+        ],
       ),
-    ]);
+    );
   }
 }
