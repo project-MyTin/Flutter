d21af8060f762bcdb47db43a44bf28e827b46306
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-05-29 16:49:04 +0900

    code : add put_motion.dart

--- lib/controllers/motion_add_controller.dart
+++ lib/controllers/motion_add_controller.dart
@@ -7,6 +7,7 @@ import 'package:mytin/models/motion_detail.dart';
 import 'package:mytin/screens/screen_routine_and_motion.dart';
 import 'package:mytin/services/get_motion_detail.dart';
 import 'package:mytin/services/post_motion.dart';
+import 'package:mytin/services/put_motion.dart';
 import 'package:mytin/utils/show_snack_bar.dart';
 
 class MotionAddController extends GetxController {
@@ -22,12 +23,14 @@ class MotionAddController extends GetxController {
   String motionDescription;
   String motionReferenceUrl;
   int motionTime;
-  var image;  // fixme : 고용량? 카메라로 찍은 사진 첨부시 413 에러 (용량때문이라 추측)
+  int motionId;
+  var image; // fixme : 고용량? 카메라로 찍은 사진 첨부시 413 에러 (용량때문이라 추측)
   MotionDetail motion;
 
   MotionAddController({bool isAdd, int motionId}) {
     this.isAdd = isAdd;
-    if(!isAdd) loadMotion(motionId);
+    this.motionId = motionId;
+    if (!isAdd) loadMotion(motionId);
     update();
   }
 
@@ -63,26 +66,25 @@ class MotionAddController extends GetxController {
   }
 
   Future<void> submit() async {
+    Map<String, dynamic> requestMap = {
+      "name": motionName,
+      "parts": [currentMotionPart],
+      "difficulty": currentDifficulty,
+      "img": image,
+      "time": motionTime,
+      "url": motionReferenceUrl,
+      "type": currentType,
+      "description": motionDescription,
+    };
     /*
      - 생성, 삭제 api로 서버와 통신하는 함수엔 await 를 걸어 동기로 처리해야함
      - 생성 페이지, 다이얼로그로 넘어가서 그런지 컨트롤러가 onDelete() 됨
           => 리스트 페이지를 들어갈 때 Get.put()으로 페이지의 컨트롤러를 수동으로 추가해야함
      - 컨트롤러에서 리스트를 새로 불러오는 함수도 수동으로 호출해야함
      */
-    if (isAdd) {
-      final res = await postMotion({
-        "name": motionName,
-        "parts": [currentMotionPart],
-        "difficulty": currentDifficulty,
-        "img": image,
-        "time": motionTime,
-        "url": motionReferenceUrl,
-        "type": currentType,
-        "description": motionDescription,
-      });
-    } else {
-      // todo 수정 api 통신
-    }
+    final response = isAdd
+        ? await postMotion(requestMap)
+        : await putMotion(motionId, requestMap);
     Get.put(MotionListController());
     Get.find<MotionListController>().loadMotions();
     Get.offAll(() => RoutineAndMotionPage(index: 1));
--- lib/services/put_motion.dart
+++ lib/services/put_motion.dart
@@ -1,11 +1,11 @@
 import 'package:dio/dio.dart';
 
-Future<int> postMotion(Map<String, dynamic> map) async {
+Future<int> putMotion(int id, Map<String, dynamic> map) async {
   final String filePath = map["img"].path;
   final String fileName = filePath.split("/").last;
   map["img"] = await MultipartFile.fromFile(filePath, filename: fileName);
-  final response = await Dio().post(
-      "http://3.34.209.123/motion",
+  final response = await Dio().put(
+      "http://3.34.209.123/motion/$id",
       data: FormData.fromMap(map)
   );
   return response.statusCode;
