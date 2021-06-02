a11410abe36354ee85b8156460a87f777364ee6d
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-06-02 20:25:45 +0900

    code : edit get_record_detail.dart

--- lib/services/record/get_record_detail.dart
+++ lib/services/record/get_record_detail.dart
@@ -8,8 +8,7 @@ Future<RecordData> loadRecordDetail(int year, int month, int day) async {
   final response = await get(
       Uri.parse("http://3.34.209.123/calendar/detail?y=$year&m=$month&d=$day"));
   final parsedRes = json.decode(response.body);
-
-  if(parsedRes["statusCode"] == "10000") {
+  if (parsedRes["statusCode"] == "10000") {
     final record = parsedRes["data"];
     return RecordData.fromMap({
       "totalTime": record["routine_total_time"],
@@ -17,23 +16,40 @@ Future<RecordData> loadRecordDetail(int year, int month, int day) async {
       "countRoutine": record["routine_num"],
       "countMotion": record["motion_num"],
       "motionList": [
-        for(var motion in record["motion_list"])
-          RecordTile.fromMap({
-            "id": motion["motion_id"],
-            "name": motion["motion_name"],
-            "imageUrl": s3Url + motion["motion_file"],
-            "time": motion["motion_time"],
-          }),
+        for (var motion in record["motion_list"])
+          motion["motion_name"] == "새로 추가됨"
+              ? RecordTile.fromMap({
+                  "id": motion["motion_id"],
+                  "name": "삭제된 동작",
+                  "imageUrl":
+                      "https://mblogthumb-phinf.pstatic.net/20161008_248/sasa9508_1475929220263OMzsO_JPEG/2.jpg?type=w2",
+                  "time": motion["motion_time"],
+                })
+              : RecordTile.fromMap({
+                  "id": motion["motion_id"],
+                  "name": motion["motion_name"],
+                  "imageUrl": s3Url + motion["motion_file"],
+                  "time": motion["motion_time"],
+                }),
       ],
       "routineList": [
-        for(var routine in record["routine_list"])
-          RecordTile.fromMap({
-            "id": routine["routine_id"],
-            "name": routine["routine_name"],
-            "imageUrl": s3Url + routine["routine_file"],
-            "time": routine["routine_time"],
-          }),
+        for (var routine in record["routine_list"])
+          routine["routine_name"] == "새로 추가됨"
+              ? RecordTile.fromMap({
+                  "id": routine["routine_id"],
+                  "name": "삭제된 루틴",
+                  "imageUrl":
+                      "https://mblogthumb-phinf.pstatic.net/20161008_248/sasa9508_1475929220263OMzsO_JPEG/2.jpg?type=w2",
+                  "time": routine["routine_time"],
+                })
+              : RecordTile.fromMap({
+                  "id": routine["routine_id"],
+                  "name": routine["routine_name"],
+                  "imageUrl": s3Url + routine["routine_file"],
+                  "time": routine["routine_time"],
+                }),
       ],
     });
-  } else return null;
+  } else
+    return null;
 }
